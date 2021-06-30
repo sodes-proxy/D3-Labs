ff1db825f4b9e92a2610812245e224ceb322a9a0
Sebastián Ochoa Osuna <85454690+sodes-proxy@users.noreply.github.com>
2021-06-30 10:08:06 -0500

    Exercise 2: "Data Binding"

---
+++ data_binding/js/main.js
@@ -0,0 +1,12 @@
+var svg = d3.select("#chart-area").append("svg")
+	.attr("width", 400)
+	.attr("height", 400);
+var data = [25, 20, 15, 10, 5];
+var rectangles=svg.selectAll("rect").data(data);
+rectangles.enter()
+    .append("rect")
+	.attr("x",5)
+	.attr("y",(d,i)=> {return (i*50)+5;})
+	.attr("width", 40)
+	.attr("height", (d)=>{return d;})
+	.attr("fill","black");
